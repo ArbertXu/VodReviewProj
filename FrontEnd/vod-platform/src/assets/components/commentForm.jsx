3762d8c8cf9a38fe74fcae6f4838c0e59dccbb40
arbert xu <arbertxu@arberts-Laptop.local>
2025-08-18 17:44:43 -0400

    comment likes now available

--- FrontEnd/vod-platform/src/assets/components/commentForm.jsx
+++ FrontEnd/vod-platform/src/assets/components/commentForm.jsx
@@ -8,7 +8,7 @@ export default function CommentSection({ vod, canComment, uploaderName, uploader
   const [comments, setComments] = useState([]);
   const [userID, setUserID] = useState(null);
   const [isCoach, setIsCoach] = useState(false);
-  const [user, setUser] = useState(false);
+  const [user, setUser] = useState(null);
   const [showAddComment, setShowAddComment] = useState(false);
   useEffect(() => {
     const storedUserID = sessionStorage.getItem("user_id");
@@ -16,6 +16,7 @@ export default function CommentSection({ vod, canComment, uploaderName, uploader
       setUserID(storedUserID);
     }
   }, []);
+
 
 
   useEffect(() => {
@@ -79,18 +80,59 @@ export default function CommentSection({ vod, canComment, uploaderName, uploader
   }
 
   const fetchComments = useCallback(async () => {
+    if (!userID || !user) return;
     try {
+      
       const res = await fetch(`${import.meta.env.VITE_API_URL}/api/vod_comments/${vod.vod_id}`);
       const data = await res.json();
-      setComments(data);
+      const token = await user.getIdToken();
+      const likeResult = await fetch(`${import.meta.env.VITE_API_URL}/api/user_comment_likes`, {
+        headers: {
+          "Authorization": `Bearer ${token}`,
+        }
+      })
+      const likedComments = await likeResult.json();
+      const combined = data.map(c => ({
+        ...c,
+        liked: likedComments.includes(c.id),
+      }));
+      setComments(combined);
     } catch (err) {
       console.error(`Error fetching comments for VOD`, err);
     }
-  }, [vod.vod_id]);
+  }, [vod.vod_id, user]);
 
   useEffect(() => {
     fetchComments();
   }, [fetchComments]);
+
+
+  const handleLike = useCallback(async (comment_id) => {
+    try {
+      const token = await user.getIdToken();
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/commentAdd/${comment_id}`, {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      })
+      if(res.ok) {
+        const data = await res.json();
+        console.log("data: ", data);
+        setComments(prevComments =>
+          prevComments.map(c =>
+            c.id === comment_id ? { ...c, likeCount: data.likeCount, liked: !c.liked } : c
+          )
+        );
+      } else {
+        const err = await res.json();
+        console.error("Error liking comment:, ", err.message);
+      }
+    } catch (error) {
+        console.error("Failed to like comment: ", error);
+      }
+  }, [user, userID, fetchComments ]);
 
   const handleSubmitComment = useCallback(async () => {
   if (!commentText.trim()) return;
@@ -200,12 +242,12 @@ export default function CommentSection({ vod, canComment, uploaderName, uploader
         <div className={`space-y-2 ${variant === "page" ? "" : " h-15 max-h-15 overflow-y-auto"}`}>
           {comments.length > 0 ? (
             comments.map((c, i) => (
-              <div key={i} className="bg-gray-800 p-1 rounded w-full">
+              <div key={c.id} className="bg-gray-800 p-1 rounded w-full">
                 <div className="flex flex-row items-center justify-center">
                   {c.user_data?.profile_img_url && (
                     <img 
                   src={c.user_data.profile_img_url}
-                  className=" size-5 rounded-full object-cover mr-1"
+                  className=" size-5 rounded-full object-cover mr-1 mb-1"
                   />
                   )}
                   {c.user_data?.username && (
@@ -213,9 +255,22 @@ export default function CommentSection({ vod, canComment, uploaderName, uploader
                   )}
                   
                 </div>
-                 <p>{c.comments}</p>
-                 <p>{c.likeCount}</p>
-                <p className="text-green-400 cursor-pointer hover:underline" onClick={() => seekTime(c.timestamp_seconds)}>{formatTimestamp(c.timestamp_seconds)} </p>
+                 <p className="mb-1">{c.comments}</p>
+                <p className="text-green-400 cursor-pointer hover:underline mb-1" onClick={() => seekTime(c.timestamp_seconds)}>{formatTimestamp(c.timestamp_seconds)} </p>
+                <div className="flex justify-center items-center mb-1">
+                    <button onClick= {() => handleLike(c.id)}>
+                      <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill={c.liked ? "red" : "white"}
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+                      </svg>
+                      </button> 
+                    <p>{c.likeCount}</p>
+                  </div>
               </div>
             ))
           ) : (
